b0215dda6ec3f2ab8636f0426197d9e583f958be
naveen <naveen9958599096@gmail.com>
2020-06-25 20:03:05 +0530

    :sparkles: updated database

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require("express")
 const dotenv = require("dotenv")
-
-//route files
-const bootcamps = require('./routes/bootcamps')
+const morgan = require("morgan")
+const connectDB = require('./config/db')
 
 //load env vars
 dotenv.config({ path: "./config/config.env" })
 
+//Database connection
+connectDB();
+
+//route files
+const bootcamps = require('./routes/bootcamps')
+
 const app = express()
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 //mount routes
 app.use('/api/v1/bootcamps', bootcamps)
